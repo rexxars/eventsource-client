04cf64257df9fae85802611e1965133b8087c0ec
Espen Hovlandsdal <espen@hovlandsdal.com>
2023-03-08 00:15:07 -0800

    test: run tests on all supported browsers by default

--- test/browser/client.browser.test.ts
+++ test/browser/client.browser.test.ts
@@ -1,3 +1,4 @@
+/* eslint-disable no-console */
 /**
  * This module:
  * - Starts a development server
@@ -8,55 +9,167 @@
  *
  * Is this weird? Yes.
  * Is there a better way? Maybe. But I haven't found one.
+ *
+ * Supported flags:
+ *
+ * --browser=firefox|chromium|webkit
+ * --no-headless
+ * --serial
  */
-import {chromium, devices} from 'playwright'
+import {chromium, firefox, webkit, BrowserType} from 'playwright'
 import {getServer} from '../server'
-import {type TestEvent, type TestEndEvent} from '../waffletest'
+import {type TestEvent} from '../waffletest'
 import {nodeReporter} from '../waffletest/reporters/nodeReporter'
 
-const {
-  onStart: reportStart,
-  onPass: reportPass,
-  onFail: reportFail,
-  onEnd: reportEnd,
-} = nodeReporter
+type BatchedResults = {
+  browserName: string
+  events: TestEvent[]
+}[]
+
+type BrowserName = 'firefox' | 'chromium' | 'webkit'
+
+const browsers: Record<BrowserName, BrowserType<{}>> = {
+  firefox,
+  chromium,
+  webkit,
+}
+
+const {onPass: reportPass, onFail: reportFail, onEnd: reportEnd} = nodeReporter
 
 const BROWSER_TEST_PORT = 3883
+const RUN_IN_SERIAL = process.argv.includes('--serial')
+const NO_HEADLESS = process.argv.includes('--no-headless')
+
+const browserFlag = getBrowserFlag()
+if (browserFlag && !isDefinedBrowserType(browserFlag)) {
+  throw new Error(`Invalid browser flag. Must be one of: ${Object.keys(browsers).join(', ')}`)
+}
+
+const browserFlagType = isDefinedBrowserType(browserFlag) ? browsers[browserFlag] : undefined
 
 // Run the tests in browsers
 ;(async function run() {
   const server = await getServer(BROWSER_TEST_PORT)
-  const browser = await chromium.launch()
-  const context = await browser.newContext(devices['iPhone 11'])
-  const page = await context.newPage()
+  const jobs =
+    browserFlag && browserFlagType
+      ? [{name: browserFlag, browserType: browserFlagType}]
+      : Object.entries(browsers).map(([name, browserType]) => ({name, browserType}))
 
-  await page.exposeFunction('reportTest', (event: TestEvent) => {
-    switch (event.event) {
-      case 'start':
-        return reportStart(event)
-      case 'pass':
-        return reportPass(event)
-      case 'fail':
-        return reportFail(event)
-      case 'end':
-        reportEnd(event)
-        return onSuiteEnd(event)
-      default:
-        throw new Error(`Unexpected event: ${(event as any).event}`)
+  // Run all browsers in parallel, unless --serial is defined
+  const results: BatchedResults = []
+
+  if (RUN_IN_SERIAL) {
+    for (const job of jobs) {
+      results.push({browserName: job.name, events: await runBrowserTest(job.browserType)})
     }
-  })
+  } else {
+    results.push(
+      ...(await Promise.all(
+        jobs.map(async (job) => {
+          return {browserName: job.name, events: await runBrowserTest(job.browserType)}
+        })
+      ))
+    )
+  }
 
-  await page.goto(`http://localhost:${BROWSER_TEST_PORT}/browser-test`)
+  // Now report output to the console
+  let totalFailures = 0
+  for (const result of results) {
+    console.log(`Browser: ${result.browserName}`)
 
-  async function onSuiteEnd(result: TestEndEvent) {
-    // Teardown
-    await context.close()
-    await browser.close()
-    await server.close()
-
-    if (result.failures > 0) {
-      // eslint-disable-next-line no-process-exit
-      process.exit(1)
+    for (const event of result.events) {
+      switch (event.event) {
+        case 'start':
+          // Ignored
+          break
+        case 'pass':
+          reportPass(event)
+          break
+        case 'fail':
+          totalFailures++
+          reportFail(event)
+          break
+        case 'end':
+          reportEnd(event)
+          break
+        default:
+          throw new Error(`Unexpected event: ${(event as any).event}`)
+      }
     }
   }
+
+  const testCount = getTestCount(results)
+  console.log(`Ran ${testCount} tests against ${jobs.length} browsers`)
+
+  await server.close()
+
+  if (totalFailures > 0) {
+    // eslint-disable-next-line no-process-exit
+    process.exit(1)
+  }
 })()
+
+function runBrowserTest(browserType: BrowserType): Promise<TestEvent[]> {
+  return new Promise(async (resolve) => {
+    const browser = await browserType.launch({headless: !NO_HEADLESS})
+    const context = await browser.newContext()
+    const page = await context.newPage()
+    const events: TestEvent[] = []
+
+    await page.exposeFunction('reportTest', async (event: TestEvent) => {
+      events.push(event)
+
+      if (event.event !== 'end') {
+        return
+      }
+
+      // Teardown
+      await context.close()
+      await browser.close()
+      resolve(events)
+    })
+
+    await page.goto(`http://localhost:${BROWSER_TEST_PORT}/browser-test`)
+  })
+}
+
+function isDefinedBrowserType(browserName: string | undefined): browserName is BrowserName {
+  return typeof browserName === 'string' && browserName in browsers
+}
+
+function getTestCount(results: BatchedResults): number {
+  for (const result of results) {
+    for (const event of result.events) {
+      if (event.event === 'start') {
+        return event.tests
+      }
+    }
+  }
+
+  return 0
+}
+
+function getBrowserFlag(): BrowserName | undefined {
+  const resolved = (function getFlag() {
+    // Look for --browser <browserName>
+    const flagIndex = process.argv.indexOf('--browser')
+    let flag = flagIndex === -1 ? undefined : process.argv[flagIndex + 1]
+    if (flag) {
+      return flag
+    }
+
+    // Look for --browser=<browserName>
+    flag = process.argv.find((arg) => arg.startsWith('--browser='))
+    return flag ? flag.split('=')[1] : undefined
+  })()
+
+  if (!resolved) {
+    return undefined
+  }
+
+  if (!isDefinedBrowserType(resolved)) {
+    throw new Error(`Invalid browser flag. Must be one of: ${Object.keys(browsers).join(', ')}`)
+  }
+
+  return resolved
+}
